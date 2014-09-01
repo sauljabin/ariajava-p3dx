5a39770fd5c995f14c152d4e9e46677bcda43dab
Saúl Piña <sauljabin@gmail.com>
2014-08-31 18:42:25 -0530

    Script para instalar ARIA

--- others/libAria/makeAndInstallLibAria.sh
+++ others/libAria/makeAndInstallLibAria.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+export JAVA_INCLUDE="/usr/lib/jvm/java-7-openjdk-i386/include"
+export JAVA_BIN="/usr/lib/jvm/java-7-openjdk-i386/bin"
 tar xzvf ARIA-2.8.1+gcc4.6.tgz
+make -C Aria-2.8.1
+make -C Aria-2.8.1 java
+make -C Aria-2.8.1/ArNetworking
+make -C Aria-2.8.1/ArNetworking java
 rm -f -R /usr/local/Aria
 make install -C Aria-2.8.1
 rm -f -R Aria-2.8.1
