9c4bdcdd5962cba400ae905b7f3f2be35027d228
Saúl Piña <sauljabin@gmail.com>
2014-12-13 22:37:08 -0530

    Agregado url a ventana de ayuda

--- src/app/gui/ViewAbout.java
+++ src/app/gui/ViewAbout.java
@@ -19,6 +19,7 @@ import javax.swing.JPanel;
 
 import app.Config;
 import app.Translate;
+import app.util.JUrlLinkLabel;
 import net.miginfocom.swing.MigLayout;
 
 public class ViewAbout extends JDialog {
@@ -26,7 +27,7 @@ public class ViewAbout extends JDialog {
 	private static final long serialVersionUID = -3296703600845483586L;
 
 	public ViewAbout() {
-		setSize(300, 170);
+		setSize(380, 200);
 		setTitle(Config.get("APP_NAME"));
 
 		JPanel panel = new JPanel(new MigLayout());
@@ -34,9 +35,10 @@ public class ViewAbout extends JDialog {
 
 		panel.add(new JLabel(Config.get("APP_NAME")), "width 300, wrap");
 		panel.add(new JLabel(Translate.get("GUI_APPDESCRIP")), "grow, wrap 20");
-		panel.add(new JLabel(Config.get("APP_LICENSE")), "grow, wrap");
-		panel.add(new JLabel(Config.get("APP_AUTHOR")), "grow, wrap");
-		
+		panel.add(new JLabel(Translate.get("GUI_LICENSE") + ": " + Config.get("APP_LICENSE")), "grow, wrap");
+		panel.add(new JLabel(Translate.get("GUI_COPYRIGHT") + ": " + Config.get("APP_AUTHOR")), "grow, wrap");
+		panel.add(new JUrlLinkLabel(Config.get("APP_URL")), "grow, wrap");
+
 		setLocationRelativeTo(this);
 		setModal(true);
 		setVisible(true);
