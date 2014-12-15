f9304594e7bafc281106e2159949d85c10fc7c4c
Sa?l Pi?a <sauljabin@gmail.com>
2014-12-15 13:17:16 -0530

    Agregado JOpenFileLabel

--- src/app/gui/ViewAbout.java
+++ src/app/gui/ViewAbout.java
@@ -13,12 +13,15 @@
 
 package app.gui;
 
+import java.io.File;
+
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import app.Config;
 import app.Translate;
+import app.util.JOpenFileLabel;
 import app.util.JUrlLinkLabel;
 import net.miginfocom.swing.MigLayout;
 
@@ -33,11 +36,16 @@ public class ViewAbout extends JDialog {
 		JPanel panel = new JPanel(new MigLayout());
 		add(panel);
 
-		panel.add(new JLabel(Config.get("APP_NAME")), "width 300, wrap");
-		panel.add(new JLabel(Translate.get("GUI_APPDESCRIP")), "grow, wrap 20");
-		panel.add(new JLabel(Translate.get("GUI_LICENSE") + ": " + Config.get("APP_LICENSE")), "grow, wrap");
-		panel.add(new JLabel(Translate.get("GUI_COPYRIGHT") + ": " + Config.get("APP_AUTHOR")), "grow, wrap");
+		panel.add(new JLabel(Config.get("APP_NAME")), "span 2, wrap");
+		panel.add(new JLabel(Translate.get("GUI_APPDESCRIP")), "span 2, grow, wrap 20");
+		panel.add(new JLabel(Translate.get("GUI_LICENSE") + ": "), "grow");
+		panel.add(new JLabel(Config.get("APP_LICENSE")), "span 2, grow, wrap");
+		panel.add(new JLabel(Translate.get("GUI_COPYRIGHT") + ": "), "grow");
+		panel.add(new JLabel(Config.get("APP_AUTHOR")), "span 2, grow, wrap");
+		panel.add(new JLabel(Translate.get("GUI_URL") + ": "), "grow");
 		panel.add(new JUrlLinkLabel(Config.get("APP_URL")), "grow, wrap");
+		panel.add(new JLabel(Translate.get("GUI_DOCUMENTATION") + ": "), "grow");
+		panel.add(new JOpenFileLabel(new File(Config.get("APP_DOCUMENTACION"))), "grow, wrap");
 
 		setLocationRelativeTo(this);
 		setModal(true);
